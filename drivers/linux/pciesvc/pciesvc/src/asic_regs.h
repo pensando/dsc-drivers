341c58c319256b070f06c0f91a428aa6a803f611
Brad Smith <Brad.Smith2@amd.com>
2022-12-01 17:30:14 -0800

    pciesvc - initial version

---
+++ drivers/linux/pciesvc/pciesvc/src/asic_regs.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2021, Pensando Systems Inc.
+ */
+
+#ifndef __ASIC_REGS_H__
+#define __ASIC_REGS_H__
+
+#ifdef ASIC_CAPRI
+#include "asic_regs_capri.h"
+#endif
+#ifdef ASIC_ELBA
+#include "asic_regs_elba.h"
+#endif
+
+#endif /* __ASIC_REGS_H__ */
---
+++ drivers/linux/pciesvc/pciesvc/src/indirect.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018,2021, Pensando Systems Inc.
+ */
+
+#ifndef __INDIRECT_H__
+#define __INDIRECT_H__
+
+#include "indirect_entry.h"
+
+int pciehw_indirect_poll_init(const int port);
+int pciehw_indirect_poll(const int port);
+int pciehw_indirect_intr_init(const int port,
+                              u_int64_t msgaddr, u_int32_t msgdata);
+int pciehw_indirect_intr(const int port);
+
+void pciehw_indirect_complete(indirect_entry_t *ientry);
+
+#endif /* __INDIRECT_H__ */
